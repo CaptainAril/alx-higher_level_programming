93f9a80f9ef52679c6040aba6bd63ca27c232330
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-19 03:44:40 +0000

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
 
-from cgi import print_directory
-
-
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             printed += 1
-    except IndexError:
-        pass
+        except IndexError:
+            pass
     print()
     return printed
