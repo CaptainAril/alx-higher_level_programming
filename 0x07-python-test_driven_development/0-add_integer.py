a5997648f4e292119e5d519fa7cdceb14c58a4d7
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-08-07 18:55:26 +0000

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -27,4 +27,6 @@ def add_integer(a, b=98):
         raise TypeError ("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError ("b must be an integer")
+    if a == float('nan') or b == float('nan'):
+        raise ValueError ("cannot convert float NaN to integer")
     return (int(a) + int(b))
