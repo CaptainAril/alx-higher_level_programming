e179d9b3a04cc5b9960b9c5266f766c414e6a56b
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-08-08 12:04:51 +0000

    TDD

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,16 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """_summary_
+
+    Args:
+        first_name (str): First name.
+        last_name (str, optional): Last name. Defaults to "".
+
+    Raises:
+        TypeError: if first_name is not a string, an empty string or is None.
+        TypeError: if last_name is not a string.
+    """
     if not isinstance(first_name, str) or first_name in ("", None):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
