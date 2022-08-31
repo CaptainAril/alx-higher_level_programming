2c3f56271de95269f55c0f14086245f06ff6ba9b
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-08-31 08:07:44 +0100

    add item to JSON

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,10 +8,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-if file_exists(filename) is False:
-    with open(filename, 'w+', encoding="utf-8") as f:
-        save_to_json_file([], filename)
+if file_exists(filename):
+    json_obj = load_from_json_file(filename)
+else:
+    json_obj = []
 
-json_obj = load_from_json_file(filename)
 json_obj.extend(argv[1:])
 save_to_json_file(json_obj, filename)
