f49d4c37a5a7ad15f9837b2128b1a1ce8f9a31cd
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-08-31 07:38:14 +0100

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script adds all arguments to a Python list,
+and then save them to JSON file.
+"""
+import sys import argv
+from os.path import exists as file_exists
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+if file_exists(filename) is False:
+    with open(filename, 'w+', encoding="utf-8") as f:
+        save_to_json_file([], filename)
+
+json_obj = load_from_json_file(filename)
+for i in argv[1:]:
+    json_obj.append(i)
+save_to_json_file(json_obj, filename)
