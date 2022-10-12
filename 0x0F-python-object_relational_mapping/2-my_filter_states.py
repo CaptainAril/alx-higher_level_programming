9ac98d641240f068650f5df252cac1c931338a60
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-10-12 10:10:19 +0100

    list city objects in database

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = BIANRY '{}' ORDER BY id ASC"\
+    query = "SELECT * FROM states WHERE BINARY `name` = '{}' ORDER BY id ASC"\
         .format(state_name)
     cursor.execute(query)
     states = cursor.fetchall()
