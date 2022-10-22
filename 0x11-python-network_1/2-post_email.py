ffef36abc407a0e0f196333d34f87cd902fee661
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-10-22 12:51:13 +0100

    send a post request to a url and displasy the body

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""Sends a `POST` request to a URL and displays the body."""
 
 
 if __name__ == '__main__':
     from sys import argv
     import urllib.request
 
-
-    with urllib.request.R
+    value = {'email': argv[2]}
+    en_value = urllib.parse.urlencode(value)
+    val = en_value.encode("UTF-8")
+    request = urllib.request.Request(argv[1], data=val, method='POST')
+    with urllib.request.urlopen(request) as r:
+        body = r.open()
+    print(body.decode("UTF-8"))
