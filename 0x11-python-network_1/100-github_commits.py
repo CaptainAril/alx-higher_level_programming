469d6359c6fa73e8ab273a52a409a5f1f564db88
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-10-23 06:55:18 +0100

    list commits form a repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script lists 10 commits from a github repository
+ - takes in two arguments (repository name and owner name)
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = 'https://api.github.com/repos/{}/{}/commits'\
+        .format(argv[1], argv[2])
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            commit = commits[i]
+            sha = commit.get('sha')
+            author_name = commit.get("commit").get('author').get('name')
+            print("[{}] {}".format(sha, author_name))
+    except IndexError:
+        pass
