74059afe1db646549cd3f2c5659958ff5275c281
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-10-23 04:49:43 +0100

    sends a post reques

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,14 @@
 if __name__ == '__main__':
     import requests
     from sys import argv
-    try:
-        q = argv[1]
-    except IndexError:
-        q = ""
-    payload = {'q': q}
+    letter = "" if len(argv) == 1 else argv[1]
+    payload = {'q': letter}
     r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     try:
         j = r.json()
         if len(j) == 0:
             print("No result")
         else:
-            print("[{}] {}".format(j['id'], j['name']))
-    except requests.exceptions.InvalidJSONError:
+            print("[{}] {}".format(j.get('id'), j.get('name')))
+    except Exception:
         print("Not a valid JSON")
