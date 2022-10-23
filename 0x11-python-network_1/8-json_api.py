a4129784458f226b093eead1dc0313780ee1f153
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-10-22 20:42:53 -0700

    .

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,6 +12,10 @@ if __name__ == '__main__':
     payload = {'q': q}
     r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     try:
-        r.json()
-    except requests.exceptions.JSONDecodeError:
+        j = r.json()
+        if len(j) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(j['id'], j['name']))
+    except requests.exceptions.InvalidJSONError:
         print("Not a valid JSON")
