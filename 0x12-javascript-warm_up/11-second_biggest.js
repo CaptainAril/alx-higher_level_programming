01d1ed4c5c82499e23d2bbc5b7740c90e95536b3
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-11-29 16:18:06 +0100

    print second largest integer passed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-const sortedArray = process.argv.slice(2).sort((a, b) => a - b)
-const val = sortedArray[sortedArray.length - 2]
-console.log(isNaN(val) ? 0 : val)
+const sortedArray = process.argv.slice(2).sort((a, b) => a - b);
+const val = sortedArray[sortedArray.length - 2];
+console.log(isNaN(val) ? 0 : val);
