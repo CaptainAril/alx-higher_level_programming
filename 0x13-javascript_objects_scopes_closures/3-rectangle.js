0d39f74876d5c6c26941d8607dba1310afe19ece
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-11-30 05:19:36 +0100

    create a class Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,17 +4,17 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+      this.print = function () {
+        let char = '';
+        for (w = this.width; w > 0; w--) {
+          char += 'X';
+        }
+        for (h = this.height; h > 0; h--) {
+          console.log(char);
+        }
+      };
     } else {
       constructor();
     }
-    this.print = function () {
-      let char = '';
-      for (w = this.width; w > 0; w--) {
-        char += 'X';
-      }
-      for (h = this.height; h > 0; h--) {
-        console.log(char);
-      }
-    };
   }
 };
