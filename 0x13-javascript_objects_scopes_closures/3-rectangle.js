4228437f173aba77f6c1f5da196cda28b55ffe94
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-11-30 05:48:05 +0100

    create a class Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,6 +4,8 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+
+      // prints rectangle using character `X`
       this.print = function () {
         let char = '';
         for (w = this.width; w > 0; w--) {
@@ -12,6 +14,19 @@ module.exports = class Rectangle {
         for (h = this.height; h > 0; h--) {
           console.log(char);
         }
+      };
+
+      // exchanges the width and height of the rectangle
+      this.rotate = function () {
+        const tmp = this.height;
+        this.height = this.width;
+        this.width = tmp;
+      };
+
+      // multiplies the width and height by 2
+      this.double = function () {
+        this.height *= 2;
+        this.width *= 2;
       };
     } else {
       constructor();
