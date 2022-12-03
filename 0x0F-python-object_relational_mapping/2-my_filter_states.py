92922ffefbcd9ece6bb1ed247b181f14d2a8ed7e
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-12-03 17:01:21 +0100

    SQL injection proof

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cursor.execute(query, (argv[4],))
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
-        print(state)
+        if state[1] == argv[4]:
+            print(state)
     cursor.close()
     db.close()
