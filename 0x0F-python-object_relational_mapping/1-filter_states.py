c82e2de60f49164b58b639a48908a10dac5f1b8f
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-12-03 14:16:34 +0100

    lists states with name that begin with N from a database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""This script lists all `states` from the database `hbtn_0e_0_usa`"""
+"""This script lists all `states` that begin with `N`
+from the database `hbtn_0e_0_usa`"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
     query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
     cursor.close()
     db.close()
