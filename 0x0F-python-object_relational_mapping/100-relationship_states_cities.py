fa2d0325956cefe1e95558e357c3f1d4232e3a29
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-12-07 15:52:49 +0100

    creates tables with one to many relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Creates a State and City entry in the database"""
 
 from sys import argv
 from sqlalchemy.orm import sessionmaker
@@ -9,6 +10,8 @@ from relationship_city import City
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                                 argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # creates/persists tables in the database from class
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
