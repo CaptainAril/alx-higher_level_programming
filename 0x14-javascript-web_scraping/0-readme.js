d3d3cfa90fcea3909c7515a3229b7b7e35f754ff
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-17 23:33:15 +0100

    Reads from file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// Reads file
 
-const fs = require('fs')
-const fileName = process.argv[2]
+const fs = require('fs');
+
+const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf-8', function (error, data) {
-  console.log(error || data)
-})
+  console.log(error || data);
+});
