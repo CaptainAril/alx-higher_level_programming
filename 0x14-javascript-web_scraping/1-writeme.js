a1563e5a58b699c37a8920c904442b59e8f78054
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-17 23:47:57 +0100

    Write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileName = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(fileName, string, 'utf8', (err) => {
+  if (err)
+    console.log(err);
+})
