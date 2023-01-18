533dd142d52cfa95c707af104e8cb596de1f0d17
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-18 06:19:07 +0100

    number of task completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
+  if (error) console.log(error);
+  else {
+    const users = JSON.parse(body);
+    const obj = {};
+    let id = 0;
+    let count = 0;
+    for (const user of users) {
+      if (id < user.userId) {
+        id = user.userId;
+        count = 0;
+      }
+      if (user.userId === id && user.completed === true) {
+        count++;
+        obj[id] = count;
+      }
+    }
+    console.log(obj);
+  }
+});
