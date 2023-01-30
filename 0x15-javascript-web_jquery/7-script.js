b5d2276c0d38c1ae225159c72e81b2555d44ecbf
CaptainAril <obeemmanuelifeoluw@gmail.com>
2023-01-30 01:29:19 +0100

    API calls with AJAX

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$.ajax({
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  type: 'GET',
+  dataType: 'Json'
+}).done(function (json) {
+  $('#character').append(json.name);
+});
